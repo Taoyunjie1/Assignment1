2a4de6a3bf2172adec40f0dd855a1d5179de6919
Tao Yun Jie <[931327564@qq.com]>
2023-02-20 17:55:01 +0800

    modify

--- clean.py
+++ clean.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     parser.add_argument('contact_info_file', help='The path to the respondent_contact.csv file')
     parser.add_argument('other_info_file', help='The path to the respondent_other.csv file')
     parser.add_argument('output_file', help='The path to the output file')
-    args = parser.parse_args()
+    args = parser.parse_args(args=[])
 
     cleaned = clean(args.respondent_contact.csv,args.respondent_other.csv)
     cleaned.to_csv(args.respondent_cleaned.csv, index=False)
